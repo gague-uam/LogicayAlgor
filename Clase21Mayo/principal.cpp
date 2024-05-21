c0edd391066703048e950d3f9fd473f49974225a
gague-uam <ggpaiz@uamv.edu.ni>
2024-05-21 11:49:17 -0600

    Ejercicio 21 de Mayo

---
+++ Clase21Mayo/principal.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "variables.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    PERSON teacher;
+    PERSON students[MAXREGISTERS];
+    int op, i=0;
+    cout << "CIF: ";
+    cin >> teacher.cif;
+    cout << "Nombre: ";
+    scanf(" %[^\n]", teacher.name);
+    cout << "Apellidos: ";
+    scanf(" %[^\n]", teacher.lastName);
+
+    do
+    {
+        system("cls||clear");
+        printf("CIF: ");
+        scanf(" %[^\n]", students[i].cif);
+        printf("Nombres: ");
+        scanf(" %[^\n]", students[i].name);
+        printf("Apellidos: ");
+        scanf(" %[^\n]", students[i].lastName);
+        printf("Nota de la clase: ");
+        scanf("%d", students[i].grade);
+
+        printf("Desea ingresar otro registro \n 1. Si \n 0. Ni \n Digite su opcion: ");
+        cin >> op;
+        i++;
+
+    }while(op!=0);
+
+    printf("Datos del Profesor\n CIF: %s\n", teacher.cif);
+    printf("Nombre completo: %s %s\n", teacher.name, teacher.lastName);
+    cout << "Listado de estudiante\n";
+    for(int j = 0; j < i; j++)
+    {
+        printf("Nombre: %s \nApellidos: %s \nNota: %d\n",
+        students[j].name, students[j].lastName, students[j].grade);
+    }
+    return 0;
+}
